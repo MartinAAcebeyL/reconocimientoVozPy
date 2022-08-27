0b0bcc35426f8ba27744b4f37c37287c769a47f5
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2022-08-27 01:31:15 -0400

    se empezo con el uso del microfono

--- app/app.py
+++ app/app.py
@@ -1,11 +1,28 @@
+from operator import truediv
 from speech_recognition import AudioFile, Recognizer
+import webbrowser as wb
 
-from utils import get_audio_to_text
+from utils import *
 
 #reconecedor de audio
 reconocedor = Recognizer()
+
 harvard_audio = AudioFile('audios/harvard.wav')
+audio = get_audio(reconocedor=reconocedor, audiofile=harvard_audio,captura_inicio=4, captura_fin=3)
+texto = get_audio_to_text(reconocedor=reconocedor, audio=audio)
 
-audio = get_audio_to_text(reconocedor=reconocedor, audiofile=harvard_audio)
+print(texto)
 
-print(reconocedor.recognize_google(audio))
+#audio con ruido de martillo de fondo
+jackhammer = AudioFile('audios/jackhammer.wav')#the stale smell of old beer lingers
+audio = get_audio(reconocedor, jackhammer)
+texto = get_audio_to_text(reconocedor, audio)
+
+print(texto)
+
+#audio con ruido de martillo de fondo, esta vez usando metodo para 'eliminar' ruido de fondo
+jackhammer = AudioFile('audios/jackhammer.wav')
+audio = get_noise_audio(reconocedor, jackhammer)
+texto = get_audio_to_text(reconocedor, audio, show_all = True)
+
+print(texto)
--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,27 @@
-def get_audio_to_text(reconocedor, audiofile):
+from math import fabs
+
+
+def get_audio_to_text(reconocedor, audio, show_all = False):
+    return reconocedor.recognize_google(audio, language = 'en-IN', show_all = show_all)
+
+def get_audio(reconocedor, audiofile, captura_inicio = False, captura_fin = False):
+    
     with audiofile as source:
-        audio = reconocedor.record(source)
+        if captura_inicio or captura_fin:
+            audio = reconocedor.record(source, offset = captura_inicio, duration = captura_fin)
+        else:
+            audio = reconocedor.record(source)
+    return audio
+
+
+def get_noise_audio(reconocedor, audiofile,captura_inicio= False, captura_fin = False):
+    audio = ''
+
+    with audiofile as source:
+        reconocedor.adjust_for_ambient_noise(source)
+        if captura_inicio or captura_fin:
+            audio = reconocedor.record(source, offset=captura_inicio, duration=captura_fin)
+        else:
+            audio = reconocedor.record(source)
+    
     return audio
